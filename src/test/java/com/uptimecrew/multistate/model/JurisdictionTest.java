9cda053b0313330f067899a21735b7c6cd5f0daf
arvindYadav <arvind_yadav1@intuit.com>
2026-06-05 10:28:07 +0530

    fix/increased the branch coverage abouve threshold

--- src/test/java/com/uptimecrew/multistate/model/JurisdictionTest.java
+++ src/test/java/com/uptimecrew/multistate/model/JurisdictionTest.java
@@ -25,4 +25,44 @@ class JurisdictionTest {
         assertThrows(IllegalArgumentException.class,
             () -> new Jurisdiction("", "California", JurisdictionKind.STATE));
     }
+
+    @Test
+    void rejects_null_code() {
+        assertThrows(NullPointerException.class,
+            () -> new Jurisdiction(null, "California", JurisdictionKind.STATE));
+    }
+
+    @Test
+    void rejects_null_displayName() {
+        assertThrows(NullPointerException.class,
+            () -> new Jurisdiction("CA", null, JurisdictionKind.STATE));
+    }
+
+    @Test
+    void rejects_blank_displayName() {
+        assertThrows(IllegalArgumentException.class,
+            () -> new Jurisdiction("CA", "", JurisdictionKind.STATE));
+    }
+
+    @Test
+    void equals_and_hashCode_cover_branches() {
+        Jurisdiction a = new Jurisdiction("CA", "California", JurisdictionKind.STATE);
+        Jurisdiction same = new Jurisdiction("CA", "California", JurisdictionKind.STATE);
+        Jurisdiction diffCode = new Jurisdiction("NY", "California", JurisdictionKind.STATE);
+        Jurisdiction diffName = new Jurisdiction("CA", "Cali", JurisdictionKind.STATE);
+
+        assertEquals(a, a);
+        assertEquals(a, same);
+        assertEquals(a.hashCode(), same.hashCode());
+        assertEquals(false, a.equals(null));
+        assertEquals(false, a.equals("not a jurisdiction"));
+        assertEquals(false, a.equals(diffCode));
+        assertEquals(false, a.equals(diffName));
+    }
+
+    @Test
+    void toString_includes_fields() {
+        Jurisdiction subject = new Jurisdiction("CA", "California", JurisdictionKind.STATE);
+        assertEquals(true, subject.toString().contains("CA"));
+    }
 }
--- src/test/java/com/uptimecrew/multistate/model/WorkDayTest.java
+++ src/test/java/com/uptimecrew/multistate/model/WorkDayTest.java
@@ -41,4 +41,61 @@ class WorkDayTest {
             LocalDate.of(2026, 3, 1)
         ));
     }
+
+    @Test
+    void rejects_null_id() {
+        assertThrows(NullPointerException.class, () -> new WorkDay(
+            null, "emp_42", "CA", LocalDate.of(2026, 3, 1)));
+    }
+
+    @Test
+    void rejects_null_workerId() {
+        assertThrows(NullPointerException.class, () -> new WorkDay(
+            "wd-001", null, "CA", LocalDate.of(2026, 3, 1)));
+    }
+
+    @Test
+    void rejects_null_jurisdictionCode() {
+        assertThrows(NullPointerException.class, () -> new WorkDay(
+            "wd-001", "emp_42", null, LocalDate.of(2026, 3, 1)));
+    }
+
+    @Test
+    void rejects_blank_id() {
+        assertThrows(IllegalArgumentException.class, () -> new WorkDay(
+            "", "emp_42", "CA", LocalDate.of(2026, 3, 1)));
+    }
+
+    @Test
+    void rejects_blank_workerId() {
+        assertThrows(IllegalArgumentException.class, () -> new WorkDay(
+            "wd-001", "", "CA", LocalDate.of(2026, 3, 1)));
+    }
+
+    @Test
+    void equals_and_hashCode_cover_branches() {
+        LocalDate date = LocalDate.of(2026, 3, 1);
+        WorkDay a = new WorkDay("wd-001", "emp_42", "CA", date);
+        WorkDay same = new WorkDay("wd-001", "emp_42", "CA", date);
+        WorkDay diffId = new WorkDay("wd-002", "emp_42", "CA", date);
+        WorkDay diffWorker = new WorkDay("wd-001", "emp_43", "CA", date);
+        WorkDay diffCode = new WorkDay("wd-001", "emp_42", "NY", date);
+        WorkDay diffDate = new WorkDay("wd-001", "emp_42", "CA", date.plusDays(1));
+
+        assertEquals(a, a);
+        assertEquals(a, same);
+        assertEquals(a.hashCode(), same.hashCode());
+        assertEquals(false, a.equals(null));
+        assertEquals(false, a.equals("not a workday"));
+        assertEquals(false, a.equals(diffId));
+        assertEquals(false, a.equals(diffWorker));
+        assertEquals(false, a.equals(diffCode));
+        assertEquals(false, a.equals(diffDate));
+    }
+
+    @Test
+    void toString_includes_fields() {
+        WorkDay subject = new WorkDay("wd-001", "emp_42", "CA", LocalDate.of(2026, 3, 1));
+        assertEquals(true, subject.toString().contains("wd-001"));
+    }
 }
